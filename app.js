b6cee31f1001546ae520cd0caf1d9fdbe6104584
Sravya Vinnakota <sravyavinnakota@Sravyas-Air.attlocal.net>
2020-08-30 17:03:24 -0700

    todo-list

---
+++ app.js
@@ -0,0 +1,46 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  var today =new Date();
+  var options={
+      weekday:"long",
+      day:"numeric",
+      month:"long"
+  };
+  var day=today.toLocaleDateString("en-US",options);
+  res.render("list",{listTitle:day,newListItems:items});
+});
+app.post("/",function(req,res){
+
+     var item= req.body.newItem ;
+     items.push(item);
+    res.redirect("/");
+
+    
+});
+
+app.get("/work",function(req,res){
+
+
+  res.render("list",{listTitle:"work List",newListItems:workItems});
+});
+app.post("/work",function(req,res){
+var item=req.body.newItem;
+workItems.push(item);
+res.redirect("/");
+
+
+});
+app.listen(3000, function(){
+  console.log("Server started on port 3000.");
+});
